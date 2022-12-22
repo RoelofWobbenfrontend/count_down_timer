eaa535fc7f52d86c8a74457efd0589707ab730b4
RoelofWobben <r.wobben@home.nl>
2022-12-22 12:01:04 +0100

    layout ready Started with js

---
+++ roelof.js
@@ -0,0 +1,32 @@
+const seconds_top = document.querySelector('.seconds').querySelector('.top');
+const seconds_bottom = document.querySelector('.seconds').querySelector('.bottom') 
+
+console.log(seconds_top); 
+
+// Stop Date
+
+stopDate = new Date("2022-12-25"); 
+
+setInterval( () => {
+    //currentTime
+    const today = new Date(); 
+    // Difference between the current time and the stopTime 
+    const duration = stopDate - today; 
+    // convert the difference in seconds
+    const seconds = Math.floor(duration % (1000 * 60)/1000)
+    // Change the seconds on the screen 
+    seconds_top.textContent = seconds.toString().padStart(2,"0");
+    seconds_bottom.textContent = seconds.toString().padStart(2,"0");
+}, 1000
+)
+
+// Todo
+
+//  calculate the days and change the screen 
+// calculate the hours and change the screen 
+// calculate the minutess and change the screen 
+// Stop the counter if duration is zero and change the message 
+
+// animation 
+
+
