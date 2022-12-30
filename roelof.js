810c0a2a4b06e2d3dcd9df7a8695b312d78325d8
JoOorikK <105049939+JoOorikK@users.noreply.github.com>
2022-12-30 12:01:20 +0100

    layout ready

--- roelof.js
+++ roelof.js
@@ -1,24 +1,59 @@
 const seconds_top = document.querySelector('.seconds').querySelector('.top');
-const seconds_bottom = document.querySelector('.seconds').querySelector('.bottom') 
+const seconds_bottom = document.querySelector('.seconds').querySelector('.bottom')
 
-console.log(seconds_top); 
+const minutes = document.querySelector('.minutes');
+const hours = document.querySelector('.hours');
+const days = document.querySelector('.days'); 
 
-// Stop Date
+const test = document.querySelector('.test');
+console.log(test); 
 
-stopDate = new Date("2022-12-25"); 
+stopDate = new Date("2023-01-21"); 
 
-setInterval( () => {
+//  a click event zodat ik de animatie kan testen 
+
+test.addEventListener('click', () => {
+    
+})
+
+
+/*  for testing maar commented omdat ik animatie wil testen
+let countdown = setInterval( () => {
     //currentTime
-    const today = new Date(); 
+    const today = new Date();
     // Difference between the current time and the stopTime 
-    const duration = stopDate - today; 
+    const duration = Math.floor(stopDate - today);
+    
+    // stop the counter if the stopdate is reached
+    if (duration <= 1000){
+        clearInterval(countdown); 
+    }
+
     // convert the difference in seconds
     const seconds = Math.floor(duration % (1000 * 60)/1000)
+    // convert the difference in minutes
+    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60))
+    // convert the difference in hours 
+    const hours = Math.floor(duration % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    //convert the difference in days
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24))
     // Change the seconds on the screen 
     seconds_top.textContent = seconds.toString().padStart(2,"0");
     seconds_bottom.textContent = seconds.toString().padStart(2,"0");
+    // Change the minutes on the screen 
+    minutes_top.textContent = minutes.toString().padStart(2,"0");
+    minutes_bottom.textContent = minutes.toString().padStart(2,"0");
+    // Change the hours on the screen 
+    hours_top.textContent = hours.toString().padStart(2,"0");
+    hours_bottom.textContent = hours.toString().padStart(2,"0");
+     // Change the days on the screen 
+     days_top.textContent = days.toString().padStart(2,"0");
+     days_bottom.textContent = days.toString().padStart(2,"0")
 }, 1000
 )
+*/
+
+
 
 // Todo
 
